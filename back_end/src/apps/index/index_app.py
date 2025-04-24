64e1bf37fc22dd1b4a67c008618dd78c563273bc
whatqt <trutnev.07@list.ru>
2025-04-24 20:21:57 +0700

    Добавлено логирование: 	добавлено логирование:   main.py 	добавлено логирование:   src/apps/index/index_app.py 	настройка и инициализация логера:   src/utils/logger.py

--- back_end/src/apps/index/index_app.py
+++ back_end/src/apps/index/index_app.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).parent.parent.parent))
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-
+from utils.logger import logger
 
 
 app = FastAPI()
 
-
 @app.get("/")
 async def index():
+    logger.debug(f"получен запрос в {index.__name__}")
     return JSONResponse({"response": "ok"})
