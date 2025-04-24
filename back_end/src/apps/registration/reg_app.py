1fd83927d46b30cc5d09c7417214935bf5943c78
whatqt <160521133+whatqt@users.noreply.github.com>
2025-04-24 18:32:16 +0700

    Добавлено обновление accsess токена при помощи refresh токена. + Изменена структуров файлов (#9)
    небольшие правки:   src/apps/registration/depends_func.py
    	добавлено две вьюхи: refresh и me. refresh позволяет обновить accsess токен, а me получить данные пользователя через accsess. Так же обновлены и старые вьюхи:   src/apps/registration/reg_app.py
    	CryptoPassword был переименован в CryptoData. Так же в JWToken добавлен новый метод для создания refresh и изменены текущие методы:   src/auth/utils.py
    	new file:   src/orm/postgresql/managament/users.py
    	renamed:    src/utils/postgresql/models.py -> src/orm/postgresql/models.py
    	renamed:    src/utils/postgresql/settings.py -> src/orm/postgresql/settings.py
    	new file:   src/utils/decorators.py
    	deleted:    src/utils/postgresql/managament/users.py

--- back_end/src/apps/registration/reg_app.py
+++ back_end/src/apps/registration/reg_app.py
@@ -4,17 +4,17 @@ sys.path.append(str(Path(__file__).parent.parent.parent))
 from fastapi import (
     FastAPI, Body, Depends,
     HTTPException, status,
-    Response
+    Response, Cookie, 
 )
 from fastapi.security import HTTPBearer
 from fastapi.security import HTTPAuthorizationCredentials as HTTPAuthCredentials
-from fastapi.responses import JSONResponse
-from auth.utils import AuthPassword
-from utils.postgresql.managament.users import ManageUser
-from utils.postgresql.models import Users
+from fastapi.responses import JSONResponse, RedirectResponse
+from auth.utils import CryptoData
+from orm.postgresql.managament.users import ManageUser
+from orm.postgresql.models import Users
 from auth.utils import JWToken
 from .depends_func import validate_auth_user
-from jwt.exceptions import InvalidTokenError
+from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 
 
 
@@ -26,8 +26,8 @@ app = FastAPI()
 async def get_jwt() -> JWToken:
     return JWToken()
 
-async def get_auth_pswd() -> AuthPassword:
-    return AuthPassword()
+async def get_crypto_data() -> CryptoData:
+    return CryptoData()
 
 @app.get("/")
 async def index():
@@ -36,53 +36,110 @@ async def index():
 @app.post("/create")
 async def create(
     data = Body(),
-    auth_password: AuthPassword = Depends(get_auth_pswd)
+    crypto_data: CryptoData = Depends(get_crypto_data)
 ):
-    hash_password = await auth_password.crypto_password(data["password"])
-    manage_user = ManageUser(
+    hash_password = await crypto_data.crypto_data(data["password"])
+    user = await ManageUser.create(
         data["email"],
         data["username"],
         hash_password
     )
-    print(hash_password)
-    user = await manage_user.create()
-    return JSONResponse({"response": f"{user}"})
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail="Такой пользователь уже существует"
+        )
+    return JSONResponse({"response": f"Пользователь успешно создан"})
 
 @app.post("/login")
 async def login(
-    response: Response, 
-    user: Users = Depends(validate_auth_user)
-):
-    jwt_token = JWToken()
-    jwt_payload = {
+    user: Users = Depends(validate_auth_user),
+    jwt_token: JWToken = Depends(JWToken),
+    refresh_token = Cookie(default=None),
+):  
+    if refresh_token:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="access is denied"
+        )
+    payload = {
         "sub": user.email,
         "username": user.username,
         "email": user.email,
     }
-    print(jwt_payload)
-    access_token = await jwt_token.encode(
-        jwt_payload
+    access_token = await jwt_token.create_accsses_token(
+        payload
     )
-    content = {"status": "successfully"}
-    response.headers["Authorization"] = f"Bearer {access_token}"
-    return JSONResponse(
-        content=content,
+    print(access_token)
+    refresh_token = await jwt_token.create_refresh_token(
+        {"sub": user.email}
+    )
+    await ManageUser.save_refresh_token(user, refresh_token)
+    response = JSONResponse(
+        content={"status": "successfully"},
         status_code=status.HTTP_202_ACCEPTED,
-        headers=response.headers
+        headers={"Authorization": f"Bearer {access_token}"}
+    )
+    response.set_cookie(
+        key="refresh_token", 
+        value=refresh_token,
+        httponly=True
+    )
+    print(user)
+    response.set_cookie(
+        key="email",
+        value= user.email,
+        httponly=True
+    )
+
+    return response
+
+@app.post("/refresh")
+async def refresh(
+    refresh_token = Cookie(default=None),
+    email = Cookie(default=None),
+    manage_user: ManageUser = Depends(ManageUser),
+    jwt_token: JWToken = Depends(
+        JWToken
+    ),    
+):
+    if refresh_token and email:
+        payload = await jwt_token.decode(refresh_token)
+        if payload:
+            user = await manage_user.get(email)
+            payload = {
+                "sub": user.email,
+                "username": user.username,
+                "email": user.email,
+            }
+            access_token = await jwt_token.create_accsses_token(payload)
+            print(access_token)
+            response = JSONResponse(
+                content={"status": "retry"},
+                status_code=status.HTTP_202_ACCEPTED,
+                headers={"Authorization": f"Bearer {access_token}"}
+            )
+            return response
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Invalid token"
     )
 
 @app.post("/me")
 async def about_user(
     cred: HTTPAuthCredentials = Depends(
-        HTTPBearer
+        HTTPBearer()
     ),
     jwt_token: JWToken = Depends(
         JWToken
-    )
+    ),    
 ):
     token = cred.credentials
     try:
         payload = await jwt_token.decode(token)
+    except ExpiredSignatureError as e:
+        # будет перевыпуск токена при помощи рефреш токена
+        return RedirectResponse("/registration/refresh")
     except InvalidTokenError as e:
         print(e)
         raise HTTPException(
