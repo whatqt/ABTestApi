47274afbcd5b2b83fe394d03698968631c9a3da1
whatqt <160521133+whatqt@users.noreply.github.com>
2025-05-29 19:44:21 +0700

    Сделана CRUD опреации для api_gateway_app: (#14)
    добавлены новые ручки для CRUD:   src/apps/api_gateway/api_gateway_app.py
    	исправлены предыдущие функции и созданы новые для операций: update, delete. Так же сделана валдиация процентов:   src/apps/api_gateway/depends_func.py
    	теперь ручка refresh поддерживает и остальные методы:   src/apps/registration/reg_app.py
    	добавлены CRUD методы:   src/orm/mongodb/managament/api_gateway.py

--- back_end/src/apps/api_gateway/depends_func.py
+++ back_end/src/apps/api_gateway/depends_func.py
@@ -4,10 +4,9 @@ from fastapi import (
     status,
     Request
 )
-from fastapi.responses import JSONResponse
 from utils.logger import logger
-from orm.mongodb.managament.api_gateway import ManageAPIGateway
-from orm.postgresql.managament.white_list_urls import ManageWhiteListUrls
+from json import loads
+from src.orm.mongodb.managament.api_gateway import ManageAPIGateway
 
 
 async def validate_data_from_create(body = Body()):
@@ -28,28 +27,67 @@ async def validate_data_from_create(body = Body()):
         detail="incorrect data"
     )
 
-async def get_settings_url(request: Request) -> dict:
-    id_user = request.state.payload["sub"]
-    api_gateway = ManageAPIGateway(id_user)
-    data_from_redirect_bytes: tuple = request.scope.get("headers")[-2]
-    url = data_from_redirect_bytes[1].decode()
-    settings_url = await api_gateway.get(url)
-    return settings_url
+async def validate_data_from_delete(body = Body()):
+    logger.debug(body)
+    check = body.get("main_api", None)
+    if check:
+        logger.debug("Проверка успешно пройдена")
+        return body
+    logger.debug("Проверка не пройдена")
+    raise HTTPException(
+        status_code=status.HTTP_406_NOT_ACCEPTABLE, 
+        detail="incorrect data"
+    )
 
-async def check_url_in_white_list(request: Request) -> str:
-    data_from_redirect_bytes: tuple = request.scope.get("headers")[-2]
-    element: bytes = data_from_redirect_bytes[0].decode()
-    logger.debug(element)
-    if element != "referer":
-        return JSONResponse(
-            content="bad request",
-            status_code=status.HTTP_400_BAD_REQUEST,
-        )
-    url = data_from_redirect_bytes[1].decode()
-    check_url = await ManageWhiteListUrls.get(url)
-    if not check_url:
-        return JSONResponse(
-            content="main api invalid",
-            status_code=status.HTTP_400_BAD_REQUEST
-        ) 
-    return check_url
+async def validate_data_from_update(request: Request, body: dict = Body()):
+    logger.debug(body)
+    required_keys = [
+        "main_api",
+        "first_api_response",
+        "first_api_percent",
+        "second_api_response",
+        "second_api_percent",
+    ]
+    check_percent = False
+    for key in body.keys():
+        if key in required_keys:
+            if key == "first_api_percent" or key == "second_api_percent":
+                if check_percent:
+                    continue
+                else:
+                    check_percent = True
+                    first_api_percent = int(
+                        body.get("first_api_percent", None)
+                    )
+                    second_api_percent = int(
+                        body.get("second_api_percent", None)
+                    )
+                    if not first_api_percent and not second_api_percent:
+                        continue
+                    else:
+                        payload = request.state.payload
+                        id_user = payload["sub"]
+                        api_gateway = ManageAPIGateway(
+                            id_user=id_user,
+                            main_api=body["main_api"]
+                        )
+                        if first_api_percent is None:
+                            first_api_percent = await api_gateway.get()
+                            first_api_percent = first_api_percent["first_api_percent"]
+                        elif second_api_percent is None:
+                            second_api_percent = await api_gateway.get()
+                            second_api_percent = second_api_percent["second_api_percent"]
+                        if first_api_percent+second_api_percent == 100:
+                            continue
+                        else:
+                            raise HTTPException(
+                                status_code=status.HTTP_406_NOT_ACCEPTABLE, 
+                                detail=f"first_api_percent and second_api_percent should both add up to 100 percent"
+                            )
+            continue
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE, 
+                detail=f"a field {key} with that name does not exist."
+            )
+    return body
